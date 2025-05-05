bde819be28ca8bb8d0baef405b080d9e1516051b
arpandrv <nepalarpan25@gmail.com>
2025-05-05 20:17:39 +0930

    before updating calculator page

--- hub_surveillance/urls.py
+++ hub_surveillance/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
-# Add media serving for development
+# Add media and static serving for development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
